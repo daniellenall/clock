8f6d2303e0456461c9f2fff31de791bdeca4055d
Danielle <daniellenall@Danielles-MacBook-Pro.local>
2019-02-12 14:34:54 +0000

    Added month and date display

--- scripts/clock.js
+++ scripts/clock.js
@@ -15,9 +15,15 @@ function getTime() {
 function getDate() {
     updateDateTime();
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var month = current.getMonth();
     var day = current.getDay();
+    var date = current.getDate();
     day = days[day];
+    month = months[month];
     document.getElementById("day").innerHTML = day;
+    document.getElementById("date").innerHTML = date + " " + month;
+
 }
 
 window.setInterval(getTime, 1000);
